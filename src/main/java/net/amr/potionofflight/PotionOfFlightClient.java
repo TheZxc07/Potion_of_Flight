e34b4e7f25bdddb1ea1715133ab17d6c80352ca3
Amr Elhossan <thehorse2003@gmail.com>
2023-07-05 05:45:06 -0400

    deez

---
+++ src/main/java/net/amr/potionofflight/PotionOfFlightClient.java
@@ -0,0 +1,10 @@
+package net.amr.potionofflight;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PotionOfFlightClient  implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/amr/potionofflight/mixin/ExampleMixin.java
@@ -0,0 +1,16 @@
+package net.amr.potionofflight.mixin;
+
+import net.amr.potionofflight.PotionOfFlight;
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class ExampleMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+		PotionOfFlight.LOGGER.info("This line is printed by an example mod mixin!");
+	}
+}
